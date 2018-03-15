4f8983a8cf4e65717ed68dadd56c67af26e292ea
Steinar Bragi Sigurðarson <steinar.bragi@gmail.com>
2018-03-15 22:36:44 +0100

    timeout fix

--- src/components/Mapp/Mapp.js
+++ src/components/Mapp/Mapp.js
@@ -4,7 +4,7 @@ import dot from '../../images/dot.png';
 import carte from '../../images/land.jpg';
 import { Transition } from 'react-transition-group' // ES6
 
-const duration = 1000;
+const duration = 00;
 
 const defaultLandStyle = {
   transition: `opacity ${duration}ms ease-in-out`,
@@ -19,7 +19,7 @@ const transitionStyles = {
 }
 
 const FadeLandScape = ({ in: inProp }) => (
-  <Transition in={inProp} timeout={duration}>
+  <Transition in={inProp} timeout={0}>
     {(state) => (
       <div style={{
         ...defaultLandStyle,
